a2dcaa8e4dd1ec83b3d929b50344f617e156443a
Jeremy Moon <jeremy.moon16@gmail.com>
2025-02-22 12:59:15 -0500

    add radio buttons and other input fields
    Signed-off-by: Jeremy Moon <jeremy.moon16@gmail.com>

--- front-end/app/page.tsx
+++ front-end/app/page.tsx
@@ -1,93 +1,162 @@
 "use client"
 
+// react and nextjs imports
 import { useEffect, useState } from "react";
 import { BrowserRouter, useSearchParams } from "react-router-dom";
 import axios from "axios";
 
+// shadcn/ui imports
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
+import { Input } from "@/components/ui/input";
+
+// lucide-react icon miports
+import { CircleUserRound } from "lucide-react";
+
+// user-defined imports
 import { loginToSpotify, requestToken } from "@/app/spotify.js";
+import "@/app/globals.css";
 
 const SpotifyAccount = () => {
-	const [params, setParams] = useSearchParams();
-  const [token, setToken] = useState("");
-	
-	const [username, setUsername] = useState("");
-  const [playlists, setPlaylists] = useState([]);
+  interface Playlist {
+    name: string;
+    song_count: number;
+  };
 
-  const [loaded, setLoaded] = useState(false);
+  const [params, setParams] = useSearchParams();
+  const [token, setToken] = useState<string>("");
+  
+  const [username, setUsername] = useState<string>("");
+  const [playlists, setPlaylists] = useState<Array<Playlist>>({});
+
+  const [loaded, setLoaded] = useState<boolean>(false);
 
   useEffect(() => {
     const fetchToken = async () => {
-			let code = params.get("code");
+      let code = params.get("code");
 
-			if (code) {
-				const tokenData = await requestToken(code);
-				setToken(tokenData.access_token);
-			}
+      if (code) {
+        const tokenData = await requestToken(code);
+        setToken(tokenData.access_token);
+      }
     };
 
-		fetchToken();
+    fetchToken();
   }, []);
 
   useEffect(() => {
     const fetchProfile = async () => {
-		  const profileURL = "https://api.spotify.com/v1/me";
-		  const payload = {headers: {Authorization: `Bearer ${token}`}};
+      const profileURL = "https://api.spotify.com/v1/me";
+      const payload = {headers: {Authorization: `Bearer ${token}`}};
 
-			try {
-				const response = await axios.get(profileURL, payload);
-				setUsername(response.data.display_name);
-			} catch (err) {
-				console.error(err);
-			}
+      try {
+        const response = await axios.get(profileURL, payload);
+        setUsername(response.data.display_name);
+      } catch (err) {
+        console.error(err);
+      }
 
-			const playlistsURL = "https://api.spotify.com/v1/me/playlists";
+      const playlistsURL = "https://api.spotify.com/v1/me/playlists";
 
-			try {
-				const response = await axios.get(playlistsURL, payload);
+      try {
+        const response = await axios.get(playlistsURL, payload);
 
-				const items = response.data.items;
-				let list = [];
+        const items = response.data.items;
+        let list = [];
 
-				for (let i = 0; i < items.length; i++) {
-					list.push({"name": items[i].name, "size": items[i].tracks.total});
-				}
+        for (let i = 0; i < items.length; i++) {
+          list.push({name: items[i].name, song_count: items[i].tracks.total});
+        }
 
-				setPlaylists(list);
-			} catch (err) {
-				console.log(err);
-			}
+        setPlaylists(list);
+      } catch (err) {
+        console.log(err);
+      }
 
-			setLoaded(true);
+      setLoaded(true);
     };
     
     if (token) {
-			fetchProfile();
+      fetchProfile();
     }
   }, [token]);
 
-	if (loaded) {
+  if (loaded) {
     return (
-			<div>
-				<p>Signed in as {username}</p>
-				{playlists.map((item, index) => (
-				  <p key={index}>{item.name} ({item.size} songs)</p>
-				))}
-			</div>
-		);
+      <div>
+        <p>Signed in as {username}</p>
+        <RadioGroup>
+          {playlists.map((item, index) => {
+            const option = "playlist" + index;
+
+            return (
+              <div key={index}>
+                <RadioGroupItem value={option} id={option} />
+                <Label htmlFor={option}>{item.name} ({item.song_count} songs)</Label>
+              </div>
+            );
+          })}
+        </RadioGroup>
+      </div>
+    );
   }
 
   return (
-		<button onClick={loginToSpotify}>Sign in to Spotify</button>
+    <div>
+      <Button variant="spotify"
+              onClick={loginToSpotify}
+              className="m-auto text-xl w-5xl p-8">
+        <CircleUserRound size={64} />
+        Sign in to Spotify
+      </Button>
+    </div>
   );
 };
 
 const MainPage = () => {
+  const sendExtractRequest = async (e) => {
+    e.preventDefault();
+    console.log("submit");
+  };
+
   return (
     <BrowserRouter>
-      <h1>Playlist Curator</h1>
-      <SpotifyAccount />
+      <div className="flex flex-col m-auto w-2xl">
+        <h1 className="font-black text-7xl text-center">Playlist Curator</h1>
+        <SpotifyAccount />
+        <Input placeholder="happy"
+               name="description"
+               required />
+        <Button>Extract</Button>
+      </div>
     </BrowserRouter>
   );
 };
 
 export default MainPage;
+
+/*
+
+        <Form onSubmit={sendExtractRequest}>
+          <FormField>
+            <FormItem>
+              <SpotifyAccount />
+              <FormMessage />
+            </FormItem>
+          </FormField>
+          <FormField>
+            <FormItem>
+              <FormLabel>I want all the songs that are...</FormLabel>
+              <FormControl>
+                <Input placeholder="happy" 
+                       name="description"
+                       required />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          </FormField>
+          <Button type="submit">Extract</Button>
+        </Form>
+
+*/
