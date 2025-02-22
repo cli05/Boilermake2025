b237b570b7ca1c2a27ee4fcb65e6446adc538ae5
Jeremy Moon <jeremy.moon16@gmail.com>
2025-02-22 13:05:16 -0500

    oopsies
    Signed-off-by: Jeremy Moon <jeremy.moon16@gmail.com>

--- front-end/app/spotify.js
+++ front-end/app/spotify.js
@@ -73,7 +73,7 @@ export const requestToken = async (code) => {
     method: "POST",
     headers: {
       "Content-Type": "application/x-www-form-urlencoded",
-		},
+    },
     body: new URLSearchParams({
       client_id: CLIENT_ID,
       grant_type: 'authorization_code',
