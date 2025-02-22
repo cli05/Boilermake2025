f43951c93409d26198d5821be0993a99f6457143
Jeremy Moon <jeremy.moon16@gmail.com>
2025-02-22 09:03:37 -0500

    spotify auth process is working?
    Signed-off-by: Jeremy Moon <jeremy.moon16@gmail.com>

--- front-end/app/page.tsx
+++ front-end/app/page.tsx
@@ -1,101 +1,93 @@
-import Image from "next/image";
+"use client"
 
-export default function Home() {
+import { useEffect, useState } from "react";
+import { BrowserRouter, useSearchParams } from "react-router-dom";
+import axios from "axios";
+
+import { loginToSpotify, requestToken } from "@/app/spotify.js";
+
+const SpotifyAccount = () => {
+	const [params, setParams] = useSearchParams();
+  const [token, setToken] = useState("");
+	
+	const [username, setUsername] = useState("");
+  const [playlists, setPlaylists] = useState([]);
+
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    const fetchToken = async () => {
+			let code = params.get("code");
+
+			if (code) {
+				const tokenData = await requestToken(code);
+				setToken(tokenData.access_token);
+			}
+    };
+
+		fetchToken();
+  }, []);
+
+  useEffect(() => {
+    const fetchProfile = async () => {
+		  const profileURL = "https://api.spotify.com/v1/me";
+		  const payload = {headers: {Authorization: `Bearer ${token}`}};
+
+			try {
+				const response = await axios.get(profileURL, payload);
+				setUsername(response.data.display_name);
+			} catch (err) {
+				console.error(err);
+			}
+
+			const playlistsURL = "https://api.spotify.com/v1/me/playlists";
+
+			try {
+				const response = await axios.get(playlistsURL, payload);
+
+				const items = response.data.items;
+				let list = [];
+
+				for (let i = 0; i < items.length; i++) {
+					list.push({"name": items[i].name, "size": items[i].tracks.total});
+				}
+
+				setPlaylists(list);
+			} catch (err) {
+				console.log(err);
+			}
+
+			setLoaded(true);
+    };
+    
+    if (token) {
+			fetchProfile();
+    }
+  }, [token]);
+
+	if (loaded) {
+    return (
+			<div>
+				<p>Signed in as {username}</p>
+				{playlists.map((item, index) => (
+				  <p key={index}>{item.name} ({item.size} songs)</p>
+				))}
+			</div>
+		);
+  }
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+		<button onClick={loginToSpotify}>Sign in to Spotify</button>
   );
-}
+};
+
+const MainPage = () => {
+  return (
+    <BrowserRouter>
+      <h1>Playlist Curator</h1>
+      <SpotifyAccount />
+    </BrowserRouter>
+  );
+};
+
+export default MainPage;
